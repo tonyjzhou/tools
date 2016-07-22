587c71818abdc17f8386238fd8828c033270daa6
Tony Zhou <tzhou@twitter.com>
2016-07-22 16:02:04 -0700

    REFACTOR: jenkins_slave_labels.py ADD: jenkins_master_slaves.py

--- src/jenkins_master_slaves.py
+++ src/jenkins_master_slaves.py
@@ -4,13 +4,13 @@ import argparse
 import json
 import urllib
 
-SERVICE_URL = 'https://jinkins-api.twitter.biz/api/1.0/slaves/{}/labels'
+SERVICE_URL = 'https://jinkins-api.twitter.biz/api/1.0/masters/{}/slaves'
 
 
 def main():
     args = parse_arguments()
 
-    hosts = [line.rstrip('\n') for line in open(args.slave_file)]
+    hosts = [line.rstrip('\n') for line in open(args.masters_file)]
     for host in hosts:
         if len(host) < 1: continue
         print "|{}|{}|".format(host, find_labels(host))
@@ -18,23 +18,24 @@ def main():
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description='%(prog)s looks for the labels for the slaves.')
-    parser.add_argument('--slave_file', type=str, nargs='?', default='slaves.in',
+        description='%(prog)s looks for all the slaves for the master.')
+    parser.add_argument('--masters_file', type=str, nargs='?', default="masters.in",
                         help=' (default: %(default)s)')
     return parser.parse_args()
 
 
 def find_labels(host):
     url = SERVICE_URL.format(host)
-    # print 'Retrieving', url
     uh = urllib.urlopen(url)
     data = uh.read()
-    # print 'Retrieved', len(data), 'characters'
+
     try:
         js = json.loads(str(data))
     except:
         js = None
-    return [v.get('name', 'Unknown').encode("ascii") for v in js]
+
+    # print js
+    return [v.get('hostname', 'Unknown').encode("ascii") for v in js]
 
 
 if __name__ == '__main__':
